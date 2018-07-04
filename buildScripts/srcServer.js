3cb1f0da60a137460b965f9a4ae6c48bb45dbea8
Bryan Tee <bryan.tee@xero.com>
2018-07-04 16:41:58 +1200

    added babel support
    Co-authored-by: Phil Alsford <phil.alsford@xero.com>

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var path = require('path');
+import express from 'express';
+import path from 'path';
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
